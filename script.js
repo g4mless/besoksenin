0392825ea78587d7c3d276296aafb40ddb1b7279
g4mless <g4mless@proton.me>
2025-11-16 22:04:42 +0700

    initialize while tomorrow actually monday :(

---
+++ script.js
@@ -0,0 +1,15 @@
+const 明日 = new Date();
+明日.setDate(明日.getDate() + 1);
+
+const 日 = 明日.getDay();
+let テキスト = "";
+
+if (日 === 1) {
+  テキスト = "besok senin :(";
+} else if (日 === 6 || 日 === 0) {
+  テキスト = "besok weekend :)";
+} else {
+  テキスト = "masih :(";
+}
+
+document.getElementById("text").textContent = テキスト;
